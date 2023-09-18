23f7d7d4c861bac88b3a153543209e42f75f890b
hasan kiwan <hasankiwan.action@gmail.com>
2023-09-19 02:52:27 +0300

    sa

--- views/working-hours/index.php
+++ views/working-hours/index.php
@@ -1,14 +1,20 @@
 <?php
 
-use app\models\WorkingHours;
-use yii\helpers\Html;
+use Yii;
 use yii\helpers\Url;
-use yii\grid\ActionColumn;
-use yii\grid\GridView;
+use yii\helpers\Html;
 use yii\widgets\Pjax;
+use yii\grid\GridView;
+use app\models\Employees;
+use yii\grid\ActionColumn;
+use kartik\date\DatePicker;
+use kartik\select2\Select2;
+use app\models\WorkingHours;
+use yii\helpers\ArrayHelper;
 /** @var yii\web\View $this */
 /** @var app\models\WorkingHoursSearch $searchModel */
 /** @var yii\data\ActiveDataProvider $dataProvider */
+$employees=ArrayHelper::map(Employees::find()->all(), 'id', 'name');
 
 $this->title = Yii::t('app', 'Working Hours');
 $this->params['breadcrumbs'][] = $this->title;
@@ -31,11 +37,40 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
             'id',
-            'employee_id',
+          
+            [
+                'attribute' => 'employee_id', // Replace with your attribute
+                'filter' => Select2::widget([
+                    'model' => $searchModel,
+                    'language' => 'en',
+                    'attribute' => 'employee_id', // Replace with your attribute
+                    'data' => $employees,
+                    'options' => ['placeholder' => 'Select a state ...'],
+                    'pluginOptions' => [
+                        'allowClear' => true
+                    ],
+                ]),
+                'value'=> function($model){
+                   return  $model->employee->name??'';
+                },
+            ],
             'start_time',
             'end_time',
-            'note:ntext',
-            //'date',
+            // 'note:ntext',
+     
+            [
+                'attribute' => 'date', // Replace with your attribute
+                'filter' => DatePicker::widget([
+                    'model' => $searchModel,
+                    'language' => 'en',
+                    'attribute' => 'date', // Replace with your attribute
+                    'pluginOptions' => [
+                        'language' => 'en',
+                        'autoclose' => true,
+                        'format' => 'yyyy-mm-dd'
+                    ],
+                ]),
+            ],
             //'created_at',
             //'updated_at',
             [
